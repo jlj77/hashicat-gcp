0c685c853ebf93df6db2b7ffb3c55706f8fc02c7
John Jarvis <john.jarvis@somerfordassociates.com>
2024-04-18 15:21:40 +0000

    Added remote backend

---
+++ remote_backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "jarvis-somerford-tfif-training"
+    workspaces {
+      name = "hashicat-gcp"
+    }
+  }
+}
