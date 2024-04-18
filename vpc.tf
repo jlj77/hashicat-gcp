c7bea95043497647f3f1c4549f782ee47c64adf0
John Jarvis <john.jarvis@somerfordassociates.com>
2024-04-18 19:13:49 +0000

    Removed ver constraint

--- vpc.tf
+++ vpc.tf
@@ -1,45 +1,45 @@
 module "network" {
-  source  = "app.terraform.io/jarvis-somerford-tfif-training/network/google"
-  version = "1.0.0"
+    source  = "app.terraform.io/jarvis-somerford-tfif-training/network/google"
+    version = "1.0.1"
 
-  project_id   = var.project_id
-  network_name = "gaurav-network"
-  routing_mode = "GLOBAL"
+    project_id   = var.project_id
+    network_name = "gaurav-network"
+    routing_mode = "GLOBAL"
 
-  subnets = [
-    {
-      subnet_name   = "gaurav-subnet"
-      subnet_ip     = "10.100.10.0/24"
-      subnet_region = var.region
-    }
-  ]
-
-  secondary_ranges = {
-    subnet-01 = [
-      {
-        range_name    = "subnet-01-secondary-01"
-        ip_cidr_range = "192.168.64.0/24"
-      },
+    subnets = [
+        {
+           subnet_name   = "gaurav-subnet"
+           subnet_ip     = "10.100.10.0/24"
+           subnet_region = var.region
+        }
     ]
 
-    subnet-02 = []
-  }
+    secondary_ranges = {
+        subnet-01 = [
+            {
+                range_name    = "subnet-01-secondary-01"
+                ip_cidr_range = "192.168.64.0/24"
+            },
+        ]
 
-  routes = [
-    {
-      name              = "egress-internet"
-      description       = "route through IGW to access internet"
-      destination_range = "0.0.0.0/0"
-      tags              = "egress-inet"
-      next_hop_internet = "true"
-    },
-    {
-      name                   = "app-proxy"
-      description            = "route through proxy to reach app"
-      destination_range      = "10.50.10.0/24"
-      tags                   = "app-proxy"
-      next_hop_instance      = "app-proxy-instance"
-      next_hop_instance_zone = "us-west1-a"
-    },
-  ]
+        subnet-02 = []
+    }
+
+    routes = [
+        {
+            name                   = "egress-internet"
+            description            = "route through IGW to access internet"
+            destination_range      = "0.0.0.0/0"
+            tags                   = "egress-inet"
+            next_hop_internet      = "true"
+        },
+        {
+            name                   = "app-proxy"
+            description            = "route through proxy to reach app"
+            destination_range      = "10.50.10.0/24"
+            tags                   = "app-proxy"
+            next_hop_instance      = "app-proxy-instance"
+            next_hop_instance_zone = "us-west1-a"
+        },
+    ]
 }
